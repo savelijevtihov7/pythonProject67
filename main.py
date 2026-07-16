04576be1f3dc9c83cf67a3a8cb48fdf6b70d29ec
SavelijEvtihov <Savelijevtihov730@gmail.com>
2026-07-16 15:38:57 +0300

    added hello script

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-Volume = int(input('Введите размер списка'))
-a = list()
-for i in range(Volume):
-    a.append(int(input('Введите элемент списка')))
-
-print(a)
-
-a = tuple(a)
-
-print(a)
-
+print('hello world')
