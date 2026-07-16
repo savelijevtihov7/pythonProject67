c86e080d239c63b042d5a482015c5e68ad9ce2fb
SavelijEvtihov <Savelijevtihov730@gmail.com>
2026-07-16 12:55:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+Volume = int(input('Введите размер списка'))
+a = list()
+for i in range(Volume):
+    a.append(int(input('Введите элемент списка')))
+
+print(a)
+
+a = tuple(a)
+
+print(a)
+
+a.__(3)
