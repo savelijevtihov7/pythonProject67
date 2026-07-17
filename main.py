8e0340637d1cc95ed03fcf6b441cdf8b682a3881
SavelijEvtihov <Savelijevtihov730@gmail.com>
2026-07-17 15:56:45 +0300

    start working with OOP from classes

--- main.py
+++ main.py
@@ -16,16 +16,40 @@
 #
 # print(indicies)
 
-users = {}
-count_users = int(input('Введите количество пользователей'))
-for i in range(count_users):
-    users.setdefault('Names', []).append(input('Введите имя пользователя'))
-    users.setdefault('Ages', []).append(int(input('Введите возраст пользователя')))
-    users.setdefault('Cities', []).append(input('Введите город пользователя'))
+# users = {}
+# count_users = int(input('Введите количество пользователей'))
+# for i in range(count_users):
+#     users.setdefault('Names', []).append(input('Введите имя пользователя'))
+#     users.setdefault('Ages', []).append(int(input('Введите возраст пользователя')))
+#     users.setdefault('Cities', []).append(input('Введите город пользователя'))
+#
+# print(users)
+#
+# def avg_age(dict):
+#     return (sum(dict['Ages']) / len(dict['Names']))
+###$$$
+class Pet:
 
-print(users)
+    def __init__(self, name, hunger):
+        self.name = name #Имя питомца
+        self.hunger = hunger #Уровень голода
 
-def avg_age(dict):
-    return (sum(dict['Ages']) / len(dict['Names']))
+    def eat(self):
+        if self.hunger > 0:
+            self.hunger -= 10
+            print(f"{self.name} поел. Голод теперь {self.hunger}")
+        else:
+            print(f"{self.name} не хочет есть!")
 
-print(avg_age(users))
+    def say_hello(self):
+        print(f"Привет! Меня зовут {self.name}, я чувствую себя отлично")
+
+barcik = Pet("Барсик", 50)
+
+sharik = Pet("Шарик", 80)
+
+barcik.eat()
+sharik.eat()
+barcik.eat()
+
+sharik.say_hello()
