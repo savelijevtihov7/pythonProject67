efdc079ff87736a732ea0d885868a8906dbb2841
SavelijEvtihov <Savelijevtihov730@gmail.com>
2026-07-17 13:00:55 +0300

    start working with dicts

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print((1,2,3))
-a = [1,2,3]
-print(a)
+# indicies = []
+# target = int(input())
+# nums = []
+# len_nums = int(input())
+# for i in range(len_nums):
+#     nums.append(int(input()))
+#
+# for i in len(nums):
+#     c = i+1
+#     while c != len(nums) - 1:
+#         if (nums[i] + nums[c]) == target:
+#             indicies.append(i)
+#             indicies.append(c)
+#         else:
+#             c += 1
+#
+# print(indicies)
+my_dict = {}
+names = ['Vasya', 'Petya', 'Gulia']
+ages = [12, 13, 47]
+cities = ['Moscow', 'Bryansk', 'Berlin']
+my_dict.update({'names': names, 'ages': ages, 'cities': cities})
+print(my_dict)
